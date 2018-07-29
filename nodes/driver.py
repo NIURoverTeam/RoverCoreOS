c03801df3ca19482b0f8c514552362cc7f70795c
LuckierDodge <ryandlewis.rl@gmail.com>
2018-07-29 22:18:17 +0000

    Setup driver.py to receive messages from nav stack

--- nodes/driver.py
+++ nodes/driver.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 import roslib
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 roslib.load_manifest('rover_core_os')
 
 import rospy
 import tf
 
+def drive(data):
+    rospy.loginfo(data)
+
 if __name__ == '__main__':
-    logger = rospy.Publisher('logger', String, queue_size=10)
+    # Start the driver node
     rospy.init_node('driver')
+    # Start node logging
+    logger = rospy.Publisher('logger', String, queue_size=10)
     log_string = 'Starting Rover 2.0....%s' % rospy.get_time()
     rospy.loginfo(log_string)
     logger.publish(log_string)
+    # Subscribe to the Nav stack's output and wait
+    rospy.Subscriber("cmd_vel", Twist, drive)
     rospy.spin()
