a8533a3bebf4a95076220fa0c8b780eeb8b8e92e
Ryan Lewis <ryandlewis.rl@gmail.com>
2018-08-06 15:18:15 -0500

    Publishing and recieving controller info

--- nodes/base_station.py
+++ nodes/base_station.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 import roslib
+from sensor_msgs.msg import Joy
 roslib.load_manifest('rover_core_os')
 
 import rospy
 
+def controller_listener(data):
+    rospy.loginfo(data.header)
+    rospy.loginfo(data.axes)
+    rospy.loginfo(data.buttons)
+
 if __name__ == '__main__':
+    # Start the base_station node
     rospy.init_node('base_station')
+    # Start node logging
+    logger = rospy.Publisher('logger', String, queue_size=10)
+    log_string = 'Starting Rover Base Station.....%s' % rospy.get_time()
+    rospy.loginfo(log_string)
+    logger.publish(log_string)
+    # Listen for joystick input and wait
+    rospy.Subscriber("joy", Joy, controller_listener)
+    rospy.spin()
--- nodes/driver.py
+++ nodes/driver.py
@@ -2,22 +2,32 @@
 import roslib
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
 roslib.load_manifest('rover_core_os')
 
 import rospy
 import tf
 
-def drive(data):
+def navigation(data):
     rospy.loginfo("Recieved!")
+    logger.publish("Recieved!")
+
+def controller_input(data):
+    rospy.loginfo("Input!")
+    logger.publish("Input!")
+
 
 if __name__ == '__main__':
+    #Prep all the topics we want to publish/subscribe to
+    logger = rospy.Publisher('logger', String, queue_size=10)
+
+    rospy.Subscriber("cmd_vel", Twist, navigation)
+    rospy.Subscriber("joy", Joy, controller_input)
+
     # Start the driver node
     rospy.init_node('driver')
     # Start node logging
-    logger = rospy.Publisher('logger', String, queue_size=10)
     log_string = 'Starting Rover 2.0....%s' % rospy.get_time()
     rospy.loginfo(log_string)
     logger.publish(log_string)
-    # Subscribe to the Nav stack's output and wait
-    rospy.Subscriber("cmd_vel", Twist, drive)
     rospy.spin()
