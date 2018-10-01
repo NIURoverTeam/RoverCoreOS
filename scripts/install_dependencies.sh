3a43bf551d573b399193ac68cd03bae5814d17ee
Ryan Lewis <ryandlewis.rl@gmail.com>
2018-10-01 00:39:08 +0000

    Adding Google Cartographer to dependency installs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,24 @@
+# Install wstool and rosdep.
+sudo apt-get update
+sudo apt-get install -y python-wstool python-rosdep ninja-build
+
 cd ~/catkin_ws
-rosdep install --from-paths src --ignore-src -r -y
+wstool init src
+
+# Merge the cartographer_ros.rosinstall file and fetch code for dependencies.
+wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
+wstool update -t src
+
+# Install proto3.
+src/cartographer/scripts/install_proto3.sh
+
+# Install deb dependencies.
+# The command 'sudo rosdep init' will print an error if you have already
+# executed it since installing ROS. This error can be ignored.
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+
+# Build and install.
+catkin_make_isolated --install --use-ninja
+source install_isolated/setup.bash
